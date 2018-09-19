029750c9786d3cae741009523cbe68fd7d3bd63c
thaonguyennv <thaonguyen.nguyenviet@gmail.com>
2018-09-20 00:47:58 +0700

    User login log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by name: params[:session][:name].downcase
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Đăng nhập thành công"
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = "Username/password không đúng"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "Bạn đã logged out"
+    redirect_to login_path
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+    #code
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:sucess] = "Đăng ký thành công"
+      redirect_to users_path
+    else
+      flash[:sucess] = "Đăng ký thất bại"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :password, :password_confirmation
+  end
+end
